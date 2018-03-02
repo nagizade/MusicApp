51f390f13723ac304e9ffcd06ea2bcce5e9a11ae
nagizade <rossokam@gmail.com>
2018-03-02 17:00:27 +0400

    Making it working

---
+++ app/src/main/java/com/nagizade/musicapp/Song.java
@@ -0,0 +1,21 @@
+package com.nagizade.musicapp;
+
+/**
+ * Created by redShaman on 3/2/2018.
+ */
+
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
